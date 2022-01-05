9cf7845492b97d8ee4c3b0bf0e6a8a41dca1edd0
RafaelPedro-88271-UM <43791542+rafaelandrepedro@users.noreply.github.com>
2022-01-05 00:11:58 +0000

    Add files via upload

--- code/libraries and main/atuators/waterpump.cpp
+++ code/libraries and main/atuators/waterpump.cpp
@@ -1,4 +1,4 @@
-#include "heater.h"
+#include "waterpump.h"
 
 WaterPump::WaterPump(int GPIO){
 	init(GPIO);
@@ -9,22 +9,22 @@ WaterPump::~WaterPump(){
 }
 
 void WaterPump::init(int GPIO){
-	string command1 = "echo " + std::to_string(GPIO)+ " > /sys/class/gpio/export";
-	string command2 = "echo out > /sys/class/gpio/gpio" + std::to_string(GPIO)+ "/direction";
-	system(command1);
-	system(command2);
+	std::string command1 = "echo " + std::to_string(GPIO)+ " > /sys/class/gpio/export";
+	std::string command2 = "echo out > /sys/class/gpio/gpio" + std::to_string(GPIO)+ "/direction";
+	system(command1.c_str());
+	system(command2.c_str());
 	this->GPIO=GPIO;
 }
 
 void WaterPump::turnOn(){
-	string command = "echo 1 > /sys/class/gpio/" + std::to_string(GPIO)+ "/value";
-	system(command);
+	std::string command = "echo 1 > /sys/class/gpio/" + std::to_string(GPIO)+ "/value";
+	system(command.c_str());
 	pumpState=1;
 }
 
 void WaterPump::turnOff(){
-	string command = "echo 0 > /sys/class/gpio/" + std::to_string(GPIO)+ "/value";
-	system(command);
+	std::string command = "echo 0 > /sys/class/gpio/" + std::to_string(GPIO)+ "/value";
+	system(command.c_str());
 	pumpState=0;
 }
 
@@ -32,4 +32,3 @@ bool WaterPump::state(){
 	return pumpState;
 }
 
-#endif
